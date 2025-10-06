3db6dda2719abdba2decb25741be6b836239ad74
ayman ibaida <ayman.ibaida@vu.edu.au>
2025-10-06 12:11:42 +1100

    L4 button navigationbar implemented

--- app/src/main/java/com/example/mynit32132b3/UI/MainActivity.kt
+++ app/src/main/java/com/example/mynit32132b3/UI/MainActivity.kt
@@ -5,10 +5,16 @@ import android.util.Log
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.mynit32132b3.R
 import com.example.mynit32132b3.viewModels.MainActivityViewModel
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavBar: BottomNavigationView
+    private lateinit var navController: NavController
 
     val viewmodel= MainActivityViewModel()
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,8 +25,31 @@ class MainActivity : AppCompatActivity() {
         Log.d("MyApplication", "hello from main activity")
         viewmodel.loadData()
 
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+        bottomNavBar = findViewById(R.id.buttomnavbar)
+
+        bottomNavBar.setOnApplyWindowInsetsListener(null)
+        bottomNavBar.setupWithNavController(navController)
+        setupnavigation()
     }
 
+    fun setupnavigation(){
+        bottomNavBar.setOnItemSelectedListener { item ->
+            if (item.itemId != bottomNavBar.selectedItemId) {
+                navController.popBackStack(item.itemId, inclusive = false)
+                val fragmentId = when (item.itemId) {
+                    R.id.LoginItemMenu -> R.id.loginFragment
+                    R.id.DashboardItemMenu -> R.id.dashboardFragment
+                    R.id.DetailsItemMenu -> R.id.detailsFragment
+                    else -> R.id.loginFragment
+                }
+                navController.navigate(fragmentId)
+            }
+                true
+            }
 
+            }
 
-}
+        }
